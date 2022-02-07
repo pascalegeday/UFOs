84a78db8552dcb2ef04caddcfeb2f1b20db41351
Pascale Geday <pascalegeday@Pascales-Air.lan>
2022-02-07 17:56:41 -0500

    Upload filters for table

--- static/js/app.js
+++ static/js/app.js
@@ -37,10 +37,16 @@ function buildTable(data) {
       // Apply `filter` to the table data to only keep the
       // rows where the `datetime` value matches the filter value
       filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
   
      // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will
     // just be the original tableData.
     buildTable(filteredData);
-  };
+  }
+  
+  // Attach an event to listen for the form button
+  d3.selectAll("#filter-btn").on("click", handleClick);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
